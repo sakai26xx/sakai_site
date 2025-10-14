03955a9f12a7faab297924c986b08f7274e4e518
sakai <kusoyowa@gmail.com>
2025-10-14 16:41:30 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function Discord(){
+    window.open('https://discord.gg/xnkZRdBdvA','_blank')
+}
+
+function X(){
+    window.open('https://x.com/oyasumi_rororo','_blank')
+}
+
+function X_sub(){
+    window.open('https://x.com/sakai_rob','_blank')
+}
